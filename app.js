d257d34c8cf01bb4b783d7981a87c3db40a6f613
Martinochrist <martinochrist2001@gmail.com>
2023-01-13 02:59:00 -0800

    java javascript

---
+++ app.js
@@ -0,0 +1,27 @@
+// assignment on javascript
+
+// assignment one of the project
+
+var k = 3;
+var u = 5;
+var output = k + u ;
+alert(output)
+
+
+// assignment two of the project
+
+//  user input to accept name of a user
+
+var fname = prompt("Enter tour first name ")
+
+var lname = prompt("Enter your last name")
+
+var first = fname.slice(0,1)
+
+var last = lname.slice(0,1)
+
+var output = "Your userid "+"is"+ first + last 
+
+alert(output)
+
+
